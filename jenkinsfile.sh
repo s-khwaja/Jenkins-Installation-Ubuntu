c7be02a6048bc5b45006bbdf3456b9cf55ff9897
sami <sami.skhwaja@gmail.com>
2020-04-10 13:02:12 -0700

    config file has been added

---
+++ jenkinsfile.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+sudo apt-get update
+
+sudo apt-get install openjdk-8-jre -y
+
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+echo 'Jenkins installed successfully! '
